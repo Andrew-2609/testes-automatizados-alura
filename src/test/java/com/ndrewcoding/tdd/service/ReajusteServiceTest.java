e4362076bdedde192390f8159a2b9a51b38f6b31
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-13 20:49:10 -0300

    Refactor: change parameters of LocalDate.of() in ReajusteServiceTest

--- src/test/java/com/ndrewcoding/tdd/service/ReajusteServiceTest.java
+++ src/test/java/com/ndrewcoding/tdd/service/ReajusteServiceTest.java
@@ -1,6 +1,8 @@
 package test.java.com.ndrewcoding.tdd.service;
 
+import main.java.com.ndrewcoding.tdd.enums.Desempenho;
 import main.java.com.ndrewcoding.tdd.model.Funcionario;
+import main.java.com.ndrewcoding.tdd.service.ReajusteService;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -13,7 +15,7 @@ public class ReajusteServiceTest {
     @Test
     public void reajusteDeveriaSerDeTresPorCentoQuandoDesempenhoForADesejar() {
         ReajusteService reajusteService = new ReajusteService();
-        Funcionario funcionario = new Funcionario("Andrew", LocalDate.of(2003, 02, 05), new BigDecimal("2000"));
+        Funcionario funcionario = new Funcionario("Andrew", LocalDate.of(2003, 2, 5), new BigDecimal("2000"));
 
         reajusteService.concederReajuste(funcionario, Desempenho.A_DESEJAR);
 
