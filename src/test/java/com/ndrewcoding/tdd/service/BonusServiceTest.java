176bea7933e8cd34ebfcacf4192eb742befacb96
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-13 20:32:24 -0300

    Chore: copy class BonusServiceTest

---
+++ src/test/java/com/ndrewcoding/tdd/service/BonusServiceTest.java
@@ -0,0 +1,38 @@
+package test.java.com.ndrewcoding.tdd.service;
+
+import main.java.com.ndrewcoding.tdd.model.Funcionario;
+import main.java.com.ndrewcoding.tdd.service.BonusService;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BonusServiceTest {
+
+    @Test
+    void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
+
+        assertEquals(new BigDecimal("0.00"), bonus);
+    }
+
+    @Test
+    void bonusDeveriaSer10PorCentoDoSalario() {
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("2500")));
+
+        assertEquals(new BigDecimal("250.00"), bonus);
+    }
+
+    @Test
+    void bonusDeveriaSerDezPorCentoParaSalarioDeExatamente10000() {
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("10000")));
+
+        assertEquals(new BigDecimal("1000.00"), bonus);
+    }
+
+}
