788ae31c6ccf6829c5a77c66e2868ea5f5307ec8
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-13 20:10:12 -0300

    Test: implement deveriaSomarDoisNumerosPositivos() test

--- src/com/ndrewcoding/tdd/CalculadoraTest.java
+++ src/com/ndrewcoding/tdd/CalculadoraTest.java
@@ -1,2 +1,16 @@
-package com.ndrewcoding.tdd;public class CalculadoraTest {
+package com.ndrewcoding.tdd;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraTest {
+
+    @Test
+    public void deveriaSomarDoisNumerosPositivos() {
+        Calculadora calculadora = new Calculadora();
+        int soma = calculadora.somar(3, 7);
+
+        Assertions.assertEquals(10, soma);
+    }
+
 }
