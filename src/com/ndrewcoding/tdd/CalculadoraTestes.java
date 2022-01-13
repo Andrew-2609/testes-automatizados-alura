662f7d19ae222d064d58ad115285855c5dad29f2
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-13 07:57:40 -0300

    Feat: add basic implementation of CalculadoraTestes

--- src/com/ndrewcoding/tdd/CalculadoraTestes.java
+++ src/com/ndrewcoding/tdd/CalculadoraTestes.java
@@ -1,4 +1,17 @@
 package com.ndrewcoding.tdd;
 
 public class CalculadoraTestes {
+    public static void main(String[] args) {
+
+        Calculadora calc = new Calculadora();
+        int soma = calc.somar(3, 7);
+        System.out.println(soma);
+        soma = calc.somar(3, 0);
+        System.out.println(soma);
+        soma = calc.somar(0, 0);
+        System.out.println(soma);
+        soma = calc.somar(3, -9);
+        System.out.println(soma);
+
+    }
 }
