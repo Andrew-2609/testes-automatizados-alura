113ae85c8a505eff387a385319f9e5ba6a71d1e9
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-13 20:48:23 -0300

    Feat: create method concederReajuste() in ReajusteService

--- src/main/java/com/ndrewcoding/tdd/service/ReajusteService.java
+++ src/main/java/com/ndrewcoding/tdd/service/ReajusteService.java
@@ -1,4 +1,17 @@
 package main.java.com.ndrewcoding.tdd.service;
 
+import main.java.com.ndrewcoding.tdd.enums.Desempenho;
+import main.java.com.ndrewcoding.tdd.model.Funcionario;
+
+import java.math.BigDecimal;
+
 public class ReajusteService {
+
+    public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
+        if (desempenho == Desempenho.A_DESEJAR) {
+            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
+            funcionario.reajustarSalario(reajuste);
+        }
+    }
+
 }
