fa0125a7f0b21aea4bc5cf9d4db494f6935f53c0
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-13 20:40:10 -0300

    Test: implement new test in ReajusteServiceTest

--- src/test/java/com/ndrewcoding/tdd/service/ReajusteServiceTest.java
+++ src/test/java/com/ndrewcoding/tdd/service/ReajusteServiceTest.java
@@ -1,4 +1,23 @@
 package test.java.com.ndrewcoding.tdd.service;
 
+import main.java.com.ndrewcoding.tdd.model.Funcionario;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ReajusteServiceTest {
+
+    @Test
+    public void reajusteDeveriaSerDeTresPorCentoQuandoDesempenhoForADesejar() {
+        ReajusteService reajusteService = new ReajusteService();
+        Funcionario funcionario = new Funcionario("Andrew", LocalDate.of(2003, 02, 05), new BigDecimal("2000"));
+
+        reajusteService.concederReajuste(funcionario, Desempenho.A_DESEJAR);
+
+        assertEquals(new BigDecimal("2060.00"), funcionario.getSalario());
+    }
+
 }
