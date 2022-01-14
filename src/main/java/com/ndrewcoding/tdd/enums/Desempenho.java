d503a841bf0663dbdd7134ca312135d1aa1d121c
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-13 21:01:26 -0300

    Feat: create abstract method retornarPercentalDeReajuste() in Desempenho

--- src/main/java/com/ndrewcoding/tdd/enums/Desempenho.java
+++ src/main/java/com/ndrewcoding/tdd/enums/Desempenho.java
@@ -1,7 +1,26 @@
 package main.java.com.ndrewcoding.tdd.enums;
 
+import java.math.BigDecimal;
+
 public enum Desempenho {
 
-    A_DESEJAR, BOM, OTIMO
+    A_DESEJAR {
+        @Override
+        public BigDecimal retornarPercentualDeReajuste() {
+            return new BigDecimal("0.03");
+        }
+    }, BOM {
+        @Override
+        public BigDecimal retornarPercentualDeReajuste() {
+            return new BigDecimal("0.20");
+        }
+    }, OTIMO {
+        @Override
+        public BigDecimal retornarPercentualDeReajuste() {
+            return new BigDecimal("0.30");
+        }
+    };
+
+    public abstract BigDecimal retornarPercentualDeReajuste();
 
 }
