4995feeafdb69c8cebc90c5615faf16606cc74c7
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-13 21:04:01 -0300

    Refactor: refactor concederReajuste() method

--- src/main/java/com/ndrewcoding/tdd/service/ReajusteService.java
+++ src/main/java/com/ndrewcoding/tdd/service/ReajusteService.java
@@ -8,16 +8,9 @@ import java.math.BigDecimal;
 public class ReajusteService {
 
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-        if (desempenho == Desempenho.A_DESEJAR) {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-            funcionario.reajustarSalario(reajuste);
-        } else if (desempenho == Desempenho.BOM) {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
-            funcionario.reajustarSalario(reajuste);
-        } else if (desempenho == Desempenho.OTIMO) {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.30"));
-            funcionario.reajustarSalario(reajuste);
-        }
+        BigDecimal percentualDeReajuste = desempenho.retornarPercentualDeReajuste();
+        BigDecimal reajuste = funcionario.getSalario().multiply(percentualDeReajuste);
+        funcionario.reajustarSalario(reajuste);
     }
 
 }
